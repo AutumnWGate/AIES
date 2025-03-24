c42be16ecd3e1d5868de1f3edda6830ff8e73b97
ryan <9242762+ryannnnnnnnnn@user.noreply.gitee.com>
2025-03-24 18:33:57 +0800

    切换路由模式

--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import Homepage from '../HomePage/HomePage'
-import HomePage2 from '../HomePage2/HomePage2';
-import HomePage3 from '../HomePage3/HomePage3';
-import AboutUsPage from '../AboutUsPage/AboutUsPage';
-import PricingPage from '../PricingPage/PricingPage';
-import PortfolioPage from '../PortfolioPage/PortfolioPage';
-import PortfolioSinglePage from '../PortfolioSinglePage/PortfolioSinglePage';
-import TeamPage from '../TeamPage/TeamPage';
-import TeamSinglePage from '../TeamSinglePage/TeamSinglePage';
-import ServicePage from '../ServicePage/ServicePage';
-import ServiceSinglePage from '../ServiceSinglePage/ServiceSinglePage';
-import BlogPage from '../BlogPage/BlogPage';
-import BlogDetails from '../BlogDetails/BlogDetails';
-import ContactPage from '../ContactPage/ContactPage';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../HomePage/HomePage";
+import HomePage2 from "../HomePage2/HomePage2";
+import HomePage3 from "../HomePage3/HomePage3";
+import AboutUsPage from "../AboutUsPage/AboutUsPage";
+import PricingPage from "../PricingPage/PricingPage";
+import PortfolioPage from "../PortfolioPage/PortfolioPage";
+import PortfolioSinglePage from "../PortfolioSinglePage/PortfolioSinglePage";
+import TeamPage from "../TeamPage/TeamPage";
+import TeamSinglePage from "../TeamSinglePage/TeamSinglePage";
+import ServicePage from "../ServicePage/ServicePage";
+import ServiceSinglePage from "../ServiceSinglePage/ServiceSinglePage";
+import BlogPage from "../BlogPage/BlogPage";
+import BlogDetails from "../BlogDetails/BlogDetails";
+import ContactPage from "../ContactPage/ContactPage";
 
 const AllRoute = () => {
-
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="home" element={<Homepage />} />
@@ -28,19 +27,21 @@ const AllRoute = () => {
           <Route path="about" element={<AboutUsPage />} />
           <Route path="pricing" element={<PricingPage />} />
           <Route path="portfolio" element={<PortfolioPage />} />
-          <Route path="portfolio_details/:slug" element={<PortfolioSinglePage />} />
+          <Route
+            path="portfolio_details/:slug"
+            element={<PortfolioSinglePage />}
+          />
           <Route path="team" element={<TeamPage />} />
           <Route path="team-single/:slug" element={<TeamSinglePage />} />
           <Route path="service" element={<ServicePage />} />
           <Route path="service-single/:slug" element={<ServiceSinglePage />} />
           <Route path="blog" element={<BlogPage />} />
           <Route path="blog-single/:slug" element={<BlogDetails />} />
-          <Route path="contact" element={<ContactPage />} />  
+          <Route path="contact" element={<ContactPage />} />
         </Routes>
-      </BrowserRouter>
-
+      </HashRouter>
     </div>
   );
-}
+};
 
 export default AllRoute;
